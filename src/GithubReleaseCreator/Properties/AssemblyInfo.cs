dd85b889e124cd0a7eb19ba2b990bef5b6e8c1e7
dazinator <darrell.tunnell@googlemail.com>
2014-11-09 02:06:46 +0000

    Modified assembly info.

--- src/GithubReleaseCreator/Properties/AssemblyInfo.cs
+++ src/GithubReleaseCreator/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("GithubReleaseCreator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Darrell Tunnell")]
 [assembly: AssemblyProduct("GithubReleaseCreator")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
